c7911e02282ddb82bf18d9789a99f25b4e38223e
Adrian Gschwend <ktk@netlabs.org>
2017-04-09 21:24:37 +0200

    Build & run scripts

---
+++ scripts/docker.sh
@@ -0,0 +1,1 @@
+docker run --rm -ti --name tabular -v ~/workspace/zazuko/statistik-zurich/input:/data tabular /bin/bash
---
+++ scripts/run.sh
@@ -0,0 +1,2 @@
+rdf serialize hdb.csv --output-format ntriples > target/final.nt
+rdf serialize gruppenliste.csv --output-format ntriples >> target/final.nt
