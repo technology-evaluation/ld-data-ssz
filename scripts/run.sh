cf6d5e6d1ce6c1e98e89180d3de29418c75c5b39
Adrian Gschwend <ktk@netlabs.org>
2017-04-10 23:56:30 +0200

    More taxonomy & scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,9 @@
-rdf serialize input/hdb.csv --output-format ntriples > target/final.nt
-rdf serialize input/gruppenliste.csv --output-format ntriples >> target/final.nt
+rdf serialize input/hdb.csv --output-format ntriples -o target/hdb.nt
+rdf serialize input/gruppenliste.csv --output-format ntriples -o target/gruppenliste.nt
+rdf serialize input/raum.csv --output-format ntriples -o target/raum.nt
+rdf serialize input/kennzahlen.csv --output-format ntriples -o target/kennzahlen.nt
+cat target/hdb-all.nt > target/final.nt
+cat target/gruppenliste.nt >> target/final.nt
+cat target/raum.nt >> target/final.nt
+cat target/kennzahlen.nt >> target/final.nt
+rapper -i guess -o ntriples input/qb.ttl >> target/final.nt
