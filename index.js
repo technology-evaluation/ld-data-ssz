3ea8242b00ed59548afa760973520989b4f4ab89
bergi <bergi@axolotlfarm.org>
2017-08-25 00:19:48 +0200

    xlsx file import

--- index.js
+++ index.js
@@ -7,13 +7,31 @@ function convertCsvw (filename) {
   const filenameOutput = 'target/' + path.basename(filename, '.csv') + '.nt'
 
   return p.rdf.dataset().import(p.file.read(filenameMetadata).pipe(p.jsonld.parse())).then((metadata) => {
-    p.file.read(filenameInput)
+    return p.run(p.file.read(filenameInput)
       .pipe(p.csvw.parse({
         baseIRI: 'file://' + filename,
         metadata: metadata
       }))
       .pipe(p.ntriples.serialize())
-      .pipe(p.file.write(filenameOutput))
+      .pipe(p.file.write(filenameOutput)))
+  })
+}
+
+function convertXlsx (filename, sheet, metadata) {
+  const filenameInput = 'input/' + filename
+  // const filenameMetadata = filenameInput + '-metadata.json'
+  const filenameMetadata = 'input/' + metadata
+  const filenameOutput = 'target/' + path.basename(filename, '.xlsx') + '.' + sheet + '.nt'
+
+  return p.rdf.dataset().import(p.file.read(filenameMetadata).pipe(p.jsonld.parse())).then((metadata) => {
+    return p.run(p.file.read(filenameInput)
+      .pipe(p.csvw.xlsx.parse({
+        baseIRI: 'file://' + filename,
+        metadata: metadata,
+        sheet: sheet
+      }))
+      .pipe(p.ntriples.serialize())
+      .pipe(p.file.write(filenameOutput)))
   })
 }
 
@@ -21,13 +39,25 @@ const filenames = [
   'hdb.csv'
 ]
 
+const xlsxSources = [{
+  filename: 'HDB_Listen.xlsx',
+  sheet: 'Gruppenliste',
+  metadata: 'gruppenliste.csv-metadata.json'
+}]
+
 p.run(() => {
   p.shell.mkdir('-p', 'target/')
-
+}).then(() => {
   return p.Promise.serially(filenames, (filename) => {
     console.log('convert: ' + filename)
 
     return convertCsvw(filename)
+  })
+}).then(() => {
+  return p.Promise.serially(xlsxSources, (source) => {
+    console.log('convert: ' + source.filename + ' ' + source.sheet)
+
+    return convertXlsx(source.filename, source.sheet, source.metadata)
   })
 }).then(() => {
   console.log('done')
