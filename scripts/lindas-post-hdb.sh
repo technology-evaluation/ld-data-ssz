9c624ced21bf2bb8579eef878987b5eeecfaf18f
Adrian Gschwend <ktk@netlabs.org>
2018-01-06 16:34:32 +0100

    Post DataCube Metadata as well

--- scripts/lindas-post-hdb.sh
+++ scripts/lindas-post-hdb.sh
@@ -5,3 +5,9 @@ curl -n \
      -T target/hdb-clean.nt \
      -G https://test.lindas-data.ch:8443/lindas \
      --data-urlencode graph=https://linked.opendata.swiss/graph/zh/statistics
+curl -n \
+     -X POST \
+     -H Content-Type:application/n-triples \
+     -T target/hdb-meta.nt \
+     -G https://test.lindas-data.ch:8443/lindas \
+     --data-urlencode graph=https://linked.opendata.swiss/graph/zh/statistics
--- scripts/lindas-post-hdb_prod.sh
+++ scripts/lindas-post-hdb_prod.sh
@@ -5,3 +5,9 @@ curl -n \
      -T target/hdb-clean.nt \
      -G https://lindas-data.ch:8443/lindas \
      --data-urlencode graph=https://linked.opendata.swiss/graph/zh/statistics
+curl -n \
+     -X POST \
+     -H Content-Type:application/n-triples \
+     -T target/hdb-meta.nt \
+     -G https://lindas-data.ch:8443/lindas \
+     --data-urlencode graph=https://linked.opendata.swiss/graph/zh/statistics
