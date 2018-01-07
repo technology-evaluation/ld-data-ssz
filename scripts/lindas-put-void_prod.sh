06c5faf5b4ca5fc9920ebfe34cfba6f0d7e2295e
Adrian Gschwend <ktk@netlabs.org>
2018-01-07 19:14:42 +0100

    Proper date in VoID in prod

--- scripts/lindas-put-void_prod.sh
+++ scripts/lindas-put-void_prod.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+TODAY="$(date "+%Y-%m-%d")"
+cat input/void.ttl| sed "s/%%DATEISSUED%%/${TODAY}/g" > target/void.ttl
 curl -n \
      -X PUT \
      -H Content-Type:text/turtle \
-     -T input/void.ttl \
+     -T target/void.ttl \
      -G https://lindas-data.ch:8443/lindas \
      --data-urlencode graph=https://linked.opendata.swiss/graph/zh/statistics
