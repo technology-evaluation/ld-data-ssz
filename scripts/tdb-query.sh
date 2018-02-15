789b83e38a3f9f59dc783322b5463fd22a6be10c
Adrian Gschwend <ktk@netlabs.org>
2018-02-15 23:25:00 +0100

    Fixing queries

--- scripts/tdb-query.sh
+++ scripts/tdb-query.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-tdbupdate --loc=target/tdb_ssz --update=sparql/qb-slices.rq
-tdbupdate --loc=target/tdb_ssz --update=sparql/slice-observation.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/qb-slicelock.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/dataset-view.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/view-shape-dimensions.rq
