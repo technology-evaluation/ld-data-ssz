aeea4d72b343ea2b0893bd1e1e48384d04acd511
Adrian Gschwend <ktk@netlabs.org>
2018-02-20 15:26:27 +0100

    Adjustments in Data Cube

--- scripts/tdb-query.sh
+++ scripts/tdb-query.sh
@@ -2,3 +2,7 @@
 tdbupdate --loc=target/tdb_ssz --update=sparql/qb-slicelock.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/dataset-topic.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/view-shape-dimensions.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/qb-slices-code.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/delete-undefined.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/delete-xxx-object.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/delete-xxx-predicate.rq
