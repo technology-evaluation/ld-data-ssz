d54ed69e3221a4522ae7f6724430453b45f10b09
Adrian Gschwend <ktk@netlabs.org>
2018-02-20 15:39:27 +0100

    Rearranged query execution

--- scripts/stardog-query.sh
+++ scripts/stardog-query.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 stardog query ssz-staging sparql/qb-slicelock.rq
 stardog query ssz-staging sparql/dataset-topic.rq
-stardog query ssz-staging sparql/qb-slices-code.rq
 stardog query ssz-staging sparql/view-shape-dimensions.rq
 stardog query ssz-staging sparql/sliceKey-shape.rq
+stardog query ssz-staging sparql/qb-slices-code.rq
 stardog query ssz-staging sparql/delete-undefined.rq
 stardog query ssz-staging sparql/delete-xxx-object.rq
 stardog query ssz-staging sparql/delete-xxx-predicate.rq
