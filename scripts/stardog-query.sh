5f6aa2e5ba7b04d6c2b2e82a245526163424a204
Adrian Gschwend <ktk@netlabs.org>
2018-03-13 18:01:10 +0100

    Proper label for DataCube

--- scripts/stardog-query.sh
+++ scripts/stardog-query.sh
@@ -2,6 +2,7 @@
 stardog query ssz-staging sparql/raum-tree.rq
 stardog query ssz-staging sparql/qb-slicelock.rq
 stardog query ssz-staging sparql/dataset-topic.rq
+stardog query ssz-staging sparql/datacube-label.rq
 stardog query ssz-staging sparql/view-shape-dimensions.rq
 stardog query ssz-staging sparql/slice-shape.rq
 stardog query ssz-staging sparql/qb-slices-code.rq
--- scripts/tdb-query.sh
+++ scripts/tdb-query.sh
@@ -2,6 +2,7 @@
 tdbupdate --loc=target/tdb_ssz --update=sparql/raum-tree.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/qb-slicelock.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/dataset-topic.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/datacube-label.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/view-shape-dimensions.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/qb-slices-code.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/delete-undefined.rq
