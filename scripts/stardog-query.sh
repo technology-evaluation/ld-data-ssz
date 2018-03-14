b2fd617f97c3ddb5bff7f18a9a0f1474c6c733ad
Adrian Gschwend <ktk@netlabs.org>
2018-03-14 22:53:38 +0100

    Add unit to DataSet

--- scripts/stardog-query.sh
+++ scripts/stardog-query.sh
@@ -6,6 +6,7 @@ stardog query ssz-staging sparql/datacube-label.rq
 stardog query ssz-staging sparql/view-shape-dimensions.rq
 stardog query ssz-staging sparql/slice-shape.rq
 stardog query ssz-staging sparql/qb-slices-code.rq
+stardog query ssz-staging sparql/measureproperty-unit.rq
 stardog query ssz-staging sparql/delete-undefined.rq
 stardog query ssz-staging sparql/delete-xxx-object.rq
 stardog query ssz-staging sparql/delete-xxx-predicate.rq
--- scripts/tdb-query.sh
+++ scripts/tdb-query.sh
@@ -5,6 +5,7 @@ tdbupdate --loc=target/tdb_ssz --update=sparql/dataset-topic.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/datacube-label.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/view-shape-dimensions.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/qb-slices-code.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/measureproperty-unit.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/delete-undefined.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/delete-xxx-object.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/delete-xxx-predicate.rq
