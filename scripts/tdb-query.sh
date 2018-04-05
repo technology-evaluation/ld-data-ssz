4031c8aff0da99dfa96a9449c908a05dd5887105
Adrian Gschwend <ktk@netlabs.org>
2018-04-05 21:49:53 +0200

    Proper path to SPARQL queries

--- scripts/tdb-query.sh
+++ scripts/tdb-query.sh
@@ -2,9 +2,9 @@
 tdbupdate --loc=target/tdb_ssz --update=sparql/raum-tree.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/raum-collection.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/raum-raumebene.rq
-tdbupdate --loc=target/tdb_ssz --update=sparql/sparql/themenbaum-level1-3.rq
-tdbupdate --loc=target/tdb_ssz --update=sparql/sparql/themenbaum-level1-2.rq
-tdbupdate --loc=target/tdb_ssz --update=sparql/sparql/themenbaum-level1.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/themenbaum-level1-3.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/themenbaum-level1-2.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/themenbaum-level1.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/qb-slicelock.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/dataset-topic.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/datacube-label.rq
