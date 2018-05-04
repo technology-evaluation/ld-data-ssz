2e88df8bb94aac14b6dcf3485f883fcfe9e9cec1
Adrian Gschwend <ktk@netlabs.org>
2018-05-04 15:40:49 +0200

    Hopefull correct refactoring for shapes in Fuseki, plus much faster

--- scripts/tdb-query.sh
+++ scripts/tdb-query.sh
@@ -9,6 +9,7 @@ tdbupdate --loc=target/tdb_ssz --update=sparql/qb-slicelock.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/dataset-topic.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/datacube-label.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/view-shape-dimensions.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/view-shape-minmax.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/slice-shape.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/qb-slices-code.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/measureproperty-unit.rq
