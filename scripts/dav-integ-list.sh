003af5d83a9dc8e84304287cba58cf6200bc8906
Adrian Gschwend <ktk@netlabs.org>
2018-05-07 11:55:52 +0200

    WebDAV Listing

---
+++ scripts/dav-integ-list.sh
@@ -0,0 +1,6 @@
+curl --proxy ld.stadt-zuerich.ch:56948 -n -k -i -X PROPFIND https://ssz-webdav.integ.stadt-zuerich.ch/HDB_Dropzone/ --upload-file - -H "Depth: 1" <<end
+<?xml version="1.0"?>
+<a:propfind xmlns:a="DAV:">
+<a:prop><a:resourcetype/></a:prop>
+</a:propfind>
+end
