b8a2eb6694e6a5ba81efdfc592500745568e8a7a
Adrian Gschwend <ktk@netlabs.org>
2018-05-07 19:05:49 +0200

    Gruppenliste Updates, Issue #88 #89

--- scripts/tdb-query.sh
+++ scripts/tdb-query.sh
@@ -5,6 +5,7 @@ tdbupdate --loc=target/tdb_ssz --update=sparql/raum-raumebene.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/themenbaum-level1-3.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/themenbaum-level1-2.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/themenbaum-level1.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/gruppenliste-hierarchie.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/qb-slicelock.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/dataset-topic.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/datacube-label.rq
