c6cb1895c44bf0f24b7811fc9d54f3e08ad6a06a
Adrian Gschwend <ktk@netlabs.org>
2018-05-11 15:56:44 +0200

    Split Update, issue #85

--- scripts/tdb-query.sh
+++ scripts/tdb-query.sh
@@ -9,13 +9,14 @@ tdbupdate --loc=target/tdb_ssz --update=sparql/gruppenliste-hierarchie.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/qb-slicelock.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/dataset-topic.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/datacube-label.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/quelle.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/glossar.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/update.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/view-shape-dimensions.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/view-shape-minmax.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/slice-shape.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/qb-slices-code.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/measureproperty-unit.rq
-tdbupdate --loc=target/tdb_ssz --update=sparql/quelle.rq
-tdbupdate --loc=target/tdb_ssz --update=sparql/glossar.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/delete-undefined.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/delete-xxx-object.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/delete-xxx-predicate.rq
