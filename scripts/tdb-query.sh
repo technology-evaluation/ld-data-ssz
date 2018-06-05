557c3b0630c4aba94d010fe6e0f2b4e4a10f6264
Adrian Gschwend <ktk@netlabs.org>
2018-06-05 17:31:04 +0200

    Add license to slice, only CCBY so far. Issue #93

--- scripts/tdb-query.sh
+++ scripts/tdb-query.sh
@@ -17,6 +17,7 @@ tdbupdate --loc=target/tdb_ssz --update=sparql/view-shape-attributes.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/view-shape-minmax.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/view-shape-datenstand.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/view-shape-update.rq
+tdbupdate --loc=target/tdb_ssz --update=sparql/view-shape-license.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/slice-shape.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/qb-slices-code.rq
 tdbupdate --loc=target/tdb_ssz --update=sparql/measureproperty-unit.rq
