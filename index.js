a1fa31b070774fb366f47fd91882ef2c31c690d4
Adrian Gschwend <ktk@netlabs.org>
2018-08-08 15:13:03 +0200

    Code linting

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ function convertCsvw (filename, metadata) {
           const staticDimension = dimensions.length > 0 ? 'RAUM-ZEIT-' : 'RAUM-ZEIT'
           object = p.rdf.namedNode('https://ld.stadt-zuerich.ch/statistics/dataset/' + kennzahl + '-' + staticDimension + dimensions.join('-'))
           dimensions.unshift(kennzahl, 'RAUM', 'ZEIT')
+          console.log(dimensions)
           qbDataSet.set(object.value, dimensions)
         }
 
