9fa12de1ca3f594d5fd1959ba118d48059a9b4b7
Adrian Gschwend <ktk@netlabs.org>
2018-08-24 10:26:52 +0100

    Temporarily disable sameAs link to STIP, on SSZ request

--- index.js
+++ index.js
@@ -108,7 +108,8 @@ function convertCsvw (filename, metadata) {
       dataset.add(p.rdf.quad(datasetNode, p.rdf.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), p.rdf.namedNode('http://purl.org/linked-data/cube#DataSet')))
       dataset.add(p.rdf.quad(datasetNode, p.rdf.namedNode('http://www.w3.org/2004/02/skos/core#notation'), p.rdf.literal(datasetNotation)))
       dataset.add(p.rdf.quad(datasetNode, p.rdf.namedNode('http://purl.org/linked-data/cube#structure'), dsdNode))
-      dataset.add(p.rdf.quad(datasetNode, p.rdf.namedNode('http://www.w3.org/2002/07/owl#sameAs'), dataSetApiNode))
+      // TODO Temporarly disabled so we don't have a direct link from the Linked Data anymore
+      //      dataset.add(p.rdf.quad(datasetNode, p.rdf.namedNode('http://www.w3.org/2002/07/owl#sameAs'), dataSetApiNode))
       dataset.add(p.rdf.quad(dsdNode, p.rdf.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), p.rdf.namedNode('http://purl.org/linked-data/cube#DataStructureDefinition')))
       dataset.add(p.rdf.quad(dsdNode, p.rdf.namedNode('http://purl.org/linked-data/cube#component'), componentNode))
       dataset.add(p.rdf.quad(sliceKeyNode, p.rdf.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), p.rdf.namedNode('http://purl.org/linked-data/cube#SliceKey')))
