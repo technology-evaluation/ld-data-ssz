31675619f5bf1fe6434cb221eb5f07a7241f072d
Adrian Gschwend <ktk@netlabs.org>
2020-10-12 15:26:06 +0200

    Refactor start scripts

--- start.sh
+++ start.sh
@@ -2,13 +2,18 @@
 
 variables=()
 
+[[ -n "$ENDPOINT" ]] && variables+=(--variable="endpoint=$ENDPOINT")
 [[ -n "$INPUT_DIR" ]] && variables+=(--variable="inputDir=$INPUT_DIR")
 [[ -n "$OUTPUT" ]] && variables+=(--variable="targetFile=$OUTPUT")
-[[ -n "$MAPPINGS" ]] && variables+=(--variable="mappings=$MAPPINGS")
+[[ -n "$MAPPINGS_DIR" ]] && variables+=(--variable="mappingsDir=$MAPPINGS_DIR")
+[[ -n "$GRAPHSTORE_USERNAME" ]] && variables+=(--variable="user=$GRAPHSTORE_USERNAME")
+[[ -n "$GRAPHSTORE_PASSWORD" ]] && variables+=(--variable="password=$GRAPHSTORE_PASSWORD")
+[[ -n "$GRAPH" ]] && variables+=(--variable="graph=$GRAPH")
 
-node ./node_modules/.bin/barnard59 \
-  run \
+
+node  ./node_modules/.bin/barnard59 \
+  run -v \
   --format text/turtle \
   "${variables[@]}" \
-  "$@" \
-  --pipeline urn:pipeline:xrm#Main pipelines/main.ttl
+ --pipeline=urn:pipeline:xrm#MainFile \
+ pipelines/main.ttl
