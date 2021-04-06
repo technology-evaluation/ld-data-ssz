d534fc24797e999987b9139d501e2cc1114feed7
Adrian Gschwend <ktk@netlabs.org>
2021-04-06 15:29:02 +0800

    Fail properly

--- scripts/main.sh
+++ scripts/main.sh
@@ -4,17 +4,20 @@ echo "Working in environment: $SFTPENV"
 echo "df /upload/$SFTPENV/HDB_Full.zip" | sftp -b - statistikstadtzuerich@sftp.zazukoians.org
 if [ $? -eq 0 ]
 then
+    set -eo pipefail
     echo "File HDB_Full.zip exists, running main pipeline..."
     npm run fetch
     npm run output:file
     # TODO generate views
     # TODO mv file
+    set +eo pipefail
 else
     echo "File HDB_Full.zip does not exist, checking for diff delivery..."
 
     echo "df /upload/$SFTPENV/HDB_Diff.zip" | sftp -b - statistikstadtzuerich@sftp.zazukoians.org
     if [ $? -eq 0 ]
     then
+      set -eo pipefail
       echo "File HDB_Diff.zip exists, running diff pipeline"
       npm run fetchDiff
       # npm run output:dimensions:store
@@ -23,6 +26,7 @@ else
       # TODO DELETE
       # TODO generate views
       # TODO mv file
+      set +eo pipefail
     else
       echo "File HDB_Diff.zip does not exist either, aborting..."
       exit 1
